1fe0ecf41265ae90b3eaef7cb5dd79728718a559
Shahadat <shahadat2cp17@gmail.com>
2021-12-27 15:23:32 +0600

    c todo

--- app.js
+++ app.js
@@ -1,18 +1,89 @@
-var express = require("express")
-var app = express();
+var express=require("express");
+var mongoose=require("mongoose");
+var bodyParser=require("body-parser");
+var app=express();
 
-
-app.get("/",function(res,res){
-    res.send("<h1>Hey guys</h1>")
+app.set('view engine','ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true,useUnifiedTopology:true});
+const itemSchema={
+    name:String
+}
+const Item=mongoose.model("Item",itemSchema);
+const item1=new Item({
+    name:"Code Daily",
+});
+const item2=new Item({
+    name:"Talk Less",
+});
+const item3=new Item({
+    name:"Introvert",
+});
+const d=[item1,item2,item3];
+/*
+Item.insertMany(d,function(err)
+{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("Successfully saved items to DB");
+    }
+});
+*/
+app.get("/",function(req,res)
+{
+   // res.send("<h1>Hey guys!!</h1>");
+   Item.find({},function(err,f)
+   {
+      // console.log(f);
+      if(f.length===0)
+      {
+        Item.insertMany(d,function(err)
+        {
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log("Successfully saved items to DB");
+            }
+        });
+      res.redirect("/");
+      }
+      else{
+      res.render("list",{newListItems:f});
+      }
+   })
+  ;
 })
+app.post("/",function(req,res)
+{
+     const itemName=req.body.n;
+    //console.log(i);
+    //i1.push(i);
+    //res.render("list",{newListItem:i});
+   // res.redirect("/");
+   const item=new Item({
+       name:itemName
+   });
+item.save();
+res.redirect("/");
+});
+app.post("/delete",function(req,res)
+{
+  const check=req.body.checkbox;
+  Item.findByIdAndRemove(check,function(err)
+  {
+      if(!err)
+      {
+          console.log("Successfully deleted");
+          res.redirect("/");
+      }
+  })
+});
 
-
-
-
-
-
-
-
-app.listen(3000,function(){
-    console.log("Listening to port 3000");
+app.listen(3000,function()
+{
+    console.log("Server is listening to port 3000");
 })
